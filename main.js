b11e79b0e29bbc20ba0bc1d0033b272f21932443
alisonberkowitz <alison.berkowitz@students.olin.edu>
2014-12-16 00:20:58 -0500

    plotting all of the school districts participating in the METCO program

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 var map;
+var geocoder;
 function initialize() {
-  var mapOptions = {
-    zoom: 10,
-    center: new google.maps.LatLng(42.3581, -71.0636)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+	geocoder = new google.maps.Geocoder();
+	var mapOptions = {
+	    zoom: 10,
+	    center: new google.maps.LatLng(42.3581, -71.0636)
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+	    mapOptions);
+
+	for (var i = addresses.length - 1; i >= 0; i--) {
+		name = addresses[i].name;
+		plotAddress(addresses[i]);
+	};
+}
+
+function plotAddress (address) {
+	var marker = new google.maps.Marker({
+		        map: map,
+		        position: new google.maps.LatLng(address.lat,address.lng)
+		    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-console.log('map')
