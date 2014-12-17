fc28229370398fa558117c9546e435c141bbd172
alisonberkowitz <alison.berkowitz@students.olin.edu>
2014-12-16 20:39:01 -0500

    hovering over sidebar shows demographics

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+function showMETCO() {
+	document.getElementById('METCO-dem').style.display = "block";
+}
+function hideMETCO() {
+	document.getElementById('METCO-dem').style.display = "none";
+}
+
+function showBos() {
+	document.getElementById('boston-dem').style.display = "block";
+}
+function hideBos() {
+	document.getElementById('boston-dem').style.display = "none";
+}
+
 var map;
 var geocoder;
 function initialize() {
@@ -16,9 +30,49 @@ function initialize() {
 }
 
 function plotAddress (address) {
-	var infowindow = new google.maps.InfoWindow({
-	    content: '<h1>'+address.name+'</h1>'+
+	var information = '<h1>'+address.name+'</h1>'+
 	    	'<h3>METCO Student Enrollment: '+ address.enrollment + '</h3>'
+	if (address.demographics) {
+		console.log('demographics');
+		information += '<table>'+
+				'<caption>School District Demographics</caption>'+
+				'<tr>'+
+					'<th>Race</td>'+
+					'<th>Percent</td>'+
+				'</tr>'+
+				'<tr>'+
+					'<td>African American</td>'+
+					'<td>'+ address.demographics['African American']+'</td>'+
+				'</tr>'+
+				'<tr>'+
+					'<td>Asian</td>'+
+					'<td>'+ address.demographics['Asian']+'</td>'+
+				'</tr>'+
+				'<tr>'+
+					'<td>Hispanic</td>'+
+					'<td>'+ address.demographics['Hispanic']+'</td>'+
+				'</tr>'+
+				'<tr>'+
+					'<td>Native American</td>'+
+					'<td>'+ address.demographics['Native American']+'</td>'+
+				'</tr>'+
+				'<tr>'+
+					'<td>White</td>'+
+					'<td>'+ address.demographics['White']+'</td>'+
+				'</tr>'+
+				'<tr>'+
+					'<td>Native Hawaiian, Pacific Islander</td>'+
+					'<td>'+ address.demographics['Native Hawaiian, Pacific Islander']+'</td>'+
+				'</tr>'+
+				'<tr>'+
+					'<td>Multi-Race, Non Hispanic</td>'+
+					'<td>'+ address.demographics['Multi-Race, Non Hispanic']+'</td>'+
+				'</tr>'+
+			'</table>'
+	};
+	var infowindow = new google.maps.InfoWindow({
+	    content: information,
+	    maxWidth: 1000
 	});
 	var marker = new google.maps.Marker({
 		        map: map,
